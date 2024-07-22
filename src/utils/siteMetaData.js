1fb4d0d147bbc69be0d72864eb6142c2b9ef4b8a
Kibieptr <138297239+kibieptr@users.noreply.github.com>
2024-07-22 14:30:41 +0700

    Update: siteUrl

--- src/utils/siteMetaData.js
+++ src/utils/siteMetaData.js
@@ -6,7 +6,7 @@ const siteMetadata = {
   description: "Raza Art: Explore Creativity and Inspiration.",
   language: "en-us",
   theme: "system", // system, dark or light
-  siteUrl: "https://create-blog-with-nextjs.vercel.app", // your website URL
+  siteUrl: "https://art.razaofficial.cloud", // your website URL
   siteLogo: "/logo.png",
   socialBanner: "/social-banner.png", // add social banner in the public folder
   email: "kibieptrid@gmail.com",
